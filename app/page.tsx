51f20380f80c59e1ffb9c15c09751167cae8d6e2
Mohit Pardeshi <mohitpardeshi729@gmail.com>
2023-09-29 12:00:32 +0530

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -249,6 +249,15 @@ export default function Home() {
           />
          </div> 
         </div>
+        <div className="py-10 my-10 px-10 box-background mx-12" >
+            <div className="flex justify-evenly">
+                <img src="/snippet.png" alt="snippet-image" />
+                <div className="flex flex-col justify-center">
+                  <h1 className="text-white text-8xl font-semibold tracking-tighter">SDK</h1>
+                  <h3 className="CS-text-gradient font-semibold text-7xl italic tracking-tighter">Coming soon!</h3>
+                </div>
+             </div>
+        </div>
       </div>
     </>
   );
