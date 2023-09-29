85b43dfbe52deea649f65abfa42c9ef66c02293b
Mohit Pardeshi <mohitpardeshi729@gmail.com>
2023-09-29 14:34:42 +0530

    footer styling in work

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             <p className="my-5 text-xl">
               Seamless granular events hosting with NFT tickets.
             </p>
-            <button className="outline ">Claim Now</button>
+            <button className="cta-button px-8 py-1">Claim Now</button>
           </div>
           <div id="parent-div" className="relative w-full p-6">
             <Image
@@ -257,6 +257,50 @@ export default function Home() {
                   <h3 className="CS-text-gradient font-semibold text-7xl italic tracking-tighter">Coming soon!</h3>
                 </div>
              </div>
+        </div>
+        <div className="flex ">
+          <div className="flex flex-col">
+          <div className="">
+          <img src='./iks-logo.png' 
+            width="252"
+            height="300"
+            className=""
+            alt="I know spots logo" />
+            <p className="text-[#ffffffbf] text-m">Seamless granular events hosting with NFT tickets.</p>
+          </div>
+           <div className="flex">
+           <input className="rounded-md" />
+            <button>Subscribe</button>
+           </div>
+          <div className="flex justify-between">
+            <img src="./facebook.png" alt="facebook" />
+            <img src="./twitter.png" alt="twitter" />
+            <img src="./instagram.png" alt="instagram" />
+            <img src="./linkedin.png" alt="linkedin" />
+            <img src="./discord.png" alt="discord" />
+          </div>
+          <p>
+            @2023 All rights reserved.
+          </p>
+          </div>
+          <div className="flex justify-evenly">
+            <div>
+            Network
+            <ul>
+            <li className="tracking-tighter">Text 1</li>
+            <li className="tracking-tighter">Text 1</li>
+            </ul>
+            </div>
+            <div>
+            Network
+            <ul>
+            <li className="tracking-tighter">Text 1</li>
+            <li className="tracking-tighter">Text 1</li>
+            <li className="tracking-tighter">Text 1</li>
+            <li className="tracking-tighter">Text 1</li>
+            </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
