32a4df2072e65135e843a422d4d662d02feb8706
Ansh <anshsaxena4190@gmail.com>
2023-10-12 02:02:28 +0530

    feat: support page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,7 +1,29 @@
+import Script from "next/script";
+
 const Support = () => {
     return (
         <div>
             <p className="text-white">Support</p>
+            <div className="w-[20%]">
+                <iframe
+                    data-tally-src="https://tally.so/embed/3xDyqG?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                    loading="lazy"
+                    width="100%"
+                    height="381"
+                    // frameborder="0"
+                    // marginheight="0"
+                    // marginwidth="0"
+                    title="We'd love to help"
+                ></iframe>
+            </div>
+
+            <Script
+                id="tally-js"
+                src="https://tally.so/widgets/embed.js"
+                // onLoad={() => {
+                //     Tally.loadEmbeds();
+                // }}
+            />
         </div>
     );
 };
