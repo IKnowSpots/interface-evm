a7152fb5162b4c05c5c81f4d9742a3a37b670dea
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2023-11-05 00:44:44 +0530

    refactor: gradient added to shotlisting event page

--- app/dashboard/shortlist/events/page.tsx
+++ app/dashboard/shortlist/events/page.tsx
@@ -12,37 +12,38 @@ const Support = () => {
 
   return (
     <div className="bg-supportPage pb-12 text-white overflow-y-hidden px-8">
-    <div className="mb-8">
-        <CreateNav />
-    </div>
-      {/* <hr className="mx-4" /> */}
-      <div className="relative z-10 mt-16">
-        <div className="flex flex-col items-center">
-            <div>
-                <h3 className="text-3xl font-semibold mb-2">Shortlist</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quo ullam recusandae cum magni nulla commodi temporibus voluptatem illo voluptas?</p>
-            </div>
+        <div>
+            <div className="grad1 blur-[220px] w-[80%] h-[120vh] absolute z-[1]"></div>
         </div>
+        <div className="mb-8">
+            <CreateNav />
+        </div>
+        {/* <hr className="mx-4" /> */}
+        <div className="relative z-10 mt-16">
+            <div className="flex flex-col items-center">
+                <div>
+                    <h3 className="text-3xl font-semibold mb-2">Shortlist</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quo ullam recusandae cum magni nulla commodi temporibus voluptatem illo voluptas?</p>
+                </div>
+            </div>
 
-        <div className="flex justify-center my-24 items-center">
-            <div className="box-background w-[50%] px-6 py-12 relative z-20">
-                <div className=" flex flex-col justify-center items-center gap-20">
-                    <div className="w-[85%] flex justify-center items-center p-0.5 border-2 border-[#A0A0A038] rounded-lg">
-                        <button className={`flex justify-center items-center p-2 w-[50%] rounded-lg ${choose?"bg-transparent":"bg-[#201A24] border-2 border-[#A0A0A038]"}`} onClick={() => setChoose(false)}>Manual</button>
-                        <button className={`flex justify-center items-center p-2 w-[50%] rounded-lg ${choose?"bg-[#201A24] border-2 border-[#A0A0A038]":"bg-transparent"}`} onClick={() => setChoose(true)}>Bulk</button>
+            <div className="flex justify-center my-24 items-center">
+                <div className="box-background w-[50%] px-6 py-12 relative z-20">
+                    <div className=" flex flex-col justify-center items-center gap-20">
+                        <div className="w-[85%] flex justify-center items-center p-0.5 border-2 border-[#A0A0A038] rounded-lg">
+                            <button className={`flex justify-center items-center p-2 w-[50%] rounded-lg ${choose?"bg-transparent":"bg-[#201A24] border-2 border-[#A0A0A038]"}`} onClick={() => setChoose(false)}>Manual</button>
+                            <button className={`flex justify-center items-center p-2 w-[50%] rounded-lg ${choose?"bg-[#201A24] border-2 border-[#A0A0A038]":"bg-transparent"}`} onClick={() => setChoose(true)}>Bulk</button>
+                        </div>
+
+                        <div className="w-[85%]">
+                            {choose?<Bulk/>:<Manual/>}
+                        </div>
                     </div>
-
-                    <div className="w-[85%]">
-                        {choose?<Bulk/>:<Manual/>}
-                    </div>
-
-                    
                 </div>
             </div>
         </div>
-      </div>
 
-      <FooterSection/>
+        <FooterSection/>
     </div>
   );
 };
