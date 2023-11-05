f4d15ec8c55ad3fcbde2ffcfe4c914697b0e0208
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2023-11-05 11:58:05 +0530

    refactor: dashnav button spacing fixed

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -25,7 +25,7 @@ const DashNav = () => {
     function CreateButton() {
         return (
             <a href="/dashboard/create">
-                <div className="create-event-btn text-[0.8rem] flex justify-around w-[10.5rem] mx-auto px-4 py-2 border rounded-xl z-[10]">
+                <div className="create-event-btn text-[0.8rem] flex justify-around w-[10rem] mx-auto px-4 py-2 border rounded-xl z-[10]">
                     <Image
                         src={"/icons/qr.svg"}
                         width={20}
@@ -41,13 +41,13 @@ const DashNav = () => {
 
     return (
         <div id="dash-navbar" className="flex justify-around items-center gap-4 pt-8 pb-4 px-4 w-full">
-            <div className="w-[65%] flex justify-center">
-            <div className="flex justify-center items-center text-white h-[40px] border-white w-[20rem] bg-black  rounded-full px-2 py-1 ">
+            <div className="w-[60%] flex justify-center">
+            <div className="flex justify-center items-center text-white border-white w-[50%] bg-black  rounded-full px-2">
             <img src="/search.png" className="w-[10%]" alt="" />
             <input
                 type="text"
                 placeholder="Search events"
-                className="text-white h-[2.5rem] w-[45%] border-white bg-black rounded-full px-4 "
+                className="text-white h-[2.5rem] text-center w-[90%] rounded-full border-white bg-black px-2 "
             />
             </div>
             </div>
@@ -58,7 +58,7 @@ const DashNav = () => {
 
             <div>
                 <button
-                    className="text-base flex justify-around w-[7.5rem] mx-auto px-2 py-2 rounded-lg z-[10] ml-6 text-white font-semibold bg-[#070708] hover:bg-[#18181d] hover:border hover:border-black/50 focus:outline-none"
+                    className="text-sm flex justify-around w-[7.5rem] mx-auto px-2 py-3 rounded-lg z-[10] ml-3 text-white font-semibold bg-[#070708] hover:bg-[#18181d] hover:border hover:border-black/50 focus:outline-none"
                     onMouseEnter={() => setIsDropdownOpen(true)}
                     onMouseLeave={() => setIsDropdownOpen(false)}>Contract <span className="rotate-180">^</span> 
                 </button>
@@ -71,10 +71,10 @@ const DashNav = () => {
                 )}
             </div>
 
-            <div className="flex gap-2 w-[22%] justify-end items-center">
+            <div className="flex gap-2 w-[22%] justify-center items-center">
                 {/* <Link href="/test/events"> */}
                 <Link href={`/${username}/events`}>
-                    <div className="text-white bg-[#070708] py-2 text-base font-semibold flex items-center gap-2 pl-5 pr-3 border border-transparent rounded-full hover:bg-white hover:text-black">
+                    <div className="text-white bg-[#070708] py-2 text-sm font-semibold flex items-center gap-2 pl-5 pr-3 border border-transparent rounded-full hover:bg-white hover:text-black">
                         <p className="">Hii, @{username}</p>
                         <div className="h-[1.5rem] w-[1.5rem] grad1 rounded-full"></div>
                     </div>
