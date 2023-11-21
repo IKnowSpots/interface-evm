360001b8aecfd1e506d7e1cacd90cdc32ce57f6a
usmansalim3 <usmansalim3@gmail.com>
2023-11-21 22:27:27 +0530

    Connect wallet modal added to navbar

--- components/dashboard/claim-rewards/ClaimNavbar.tsx
+++ components/dashboard/claim-rewards/ClaimNavbar.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import WalletsProvider from "@/components/wallets";
 // import { useWallet } from "@solana/wallet-adapter-react";
 
 const Navbar = () => {
@@ -21,12 +22,12 @@ const Navbar = () => {
                 />
             </Link>
 
-            <button className="bg-white px-4 py-2 rounded-2xl text-black font-semibold">Select Wallet</button>
+            {/* <button className="bg-white px-4 py-2 rounded-2xl text-black font-semibold">Select Wallet</button> */}
 
-            {/* <p className=" text-center w-[12%] text-black   rounded-[1.5rem] ">
-                Connect Wallet
-                <WalletsProvider />
-            </p> */}
+            <p className=" text-center w-[12%] text-black   rounded-[1.5rem] ">
+                {/* Connect Wallet */}
+                <WalletsProvider/>
+            </p>
         </nav>
     );
 };
