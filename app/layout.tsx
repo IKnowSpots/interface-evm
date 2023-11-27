1f3736137c152a2af9f18bfb6a749fb581fb99bc
Tygar Decentralized <rahulkathuria008@gmail.com>
2023-11-27 17:31:22 +0530

    refactor: title added

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.ico" />
+                <title>iKnowSpots</title>
             </head>
             <body >
                 <Providers>{children}</Providers>
